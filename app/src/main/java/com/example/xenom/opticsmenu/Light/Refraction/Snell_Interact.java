0102031d385e8457c9ce6389f405dd614ff2d738
Tristan Ventura <tventura0297@gmail.com>
2017-06-06 16:40:18 -0400

    Added Partial Snell Module functionality

--- app/src/main/java/com/example/xenom/opticsmenu/Light/Refraction/Snell_Interact.java
+++ app/src/main/java/com/example/xenom/opticsmenu/Light/Refraction/Snell_Interact.java
@@ -13,6 +13,10 @@ import com.jjoe64.graphview.GridLabelRenderer;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+/**
+ * Better comments as well as a continuation document will be added
+ */
+
 public class Snell_Interact extends AppCompatActivity {
     private GraphView graph;
     private LineGraphSeries<DataPoint> series;
@@ -20,12 +24,14 @@ public class Snell_Interact extends AppCompatActivity {
     private TextView textViewN2;
     private SeekBar seekBarN1;
     private SeekBar seekBarN2;
-    double theta1;
+    double theta1 = 90; //Theta 1 will always be 90 degrees.
     double theta2;
-    double[] datapoints;
-    double slope;
-    final double [] x  = {-7.5, 7.5};
-    double [] y = {0, 0};
+    double N1 = 1.00; //Initialze both indexes of refraction to 1.00;
+    double N2 = 1.00;
+    int beamlength = 10; //Length of beam of light is 10 units.
+    double[] DataPointsRight = {0, 0};
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +40,9 @@ public class Snell_Interact extends AppCompatActivity {
 
         graph = (GraphView) findViewById(R.id.graph);
         textViewN1 = (TextView) findViewById(R.id.textN1);
-        textViewN2 = (TextView)findViewById(R.id.textN2);
+        textViewN2 = (TextView) findViewById(R.id.textN2);
         seekBarN1 = (SeekBar) findViewById(R.id.seekbarN1);
-        seekBarN2 = (SeekBar)findViewById(R.id.seekbarN2);
+        seekBarN2 = (SeekBar) findViewById(R.id.seekbarN2);
         textViewN1.setText("Index of Refraction: " + seekBarN1.getProgress());
         textViewN2.setText("Index of Refraction: " + seekBarN2.getProgress());
 
@@ -54,6 +60,8 @@ public class Snell_Interact extends AppCompatActivity {
         graph.getViewport().setMaxY(10);
         seekBarN1.setMax(300);
         seekBarN2.setMax(300);
+        series.setThickness(5);     //Set Line Thickness to 5
+
 
         //Sets background
         int imageResource = getResources().getIdentifier("@drawable/graph_background", null, getPackageName());
@@ -68,10 +76,12 @@ public class Snell_Interact extends AppCompatActivity {
 
         //Initialize series to default value;
         series = new LineGraphSeries<>(new DataPoint[]{
-                new DataPoint(-1, -1),
+                new DataPoint(0, 10),       //Hard coded here because I'm lazy
                 new DataPoint(0, 0), //We have defined the origin to be the point where the light beam changes mediums
-                new DataPoint(1, 1)
+                new DataPoint(0, -10)
         });
+
+        graph.addSeries(series);
         seekBarN1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
             int progress = 0;
@@ -79,19 +89,8 @@ public class Snell_Interact extends AppCompatActivity {
 
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                graph.removeAllSeries();
                 textViewN1.setText("Index of Refraction: " + seekBar.getProgress());
                 this.progress = progress;
-                slope = seekBar.getProgress()/100.00;
-                y[0] = slope * x[0];
-                y[1] = slope * x[1];
-
-                series = new LineGraphSeries<>(new DataPoint[]{
-                        new DataPoint(x[0], y[0]),
-                        new DataPoint(0, 0), //We have defined the origin to be the point where the light beam changes mediums
-                        new DataPoint(x[1], y[1])
-                });
-                graph.addSeries(series);
             }
 
             @Override
@@ -114,16 +113,18 @@ public class Snell_Interact extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 graph.removeAllSeries();
-                textViewN2.setText("Index of Refraction: " + seekBar.getProgress());
+                textViewN2.setText("Index of Refraction N2: " + N2);
                 this.progress = progress;
-                slope = seekBar.getProgress()/100.00;
-                y[0] = slope * x[0];
-                y[1] = slope * x[1];
+                setN2(seekBarN2.getProgress() / 100.0 + 1.00);
+                setThetaTwo();
+
+                DataPointsRight = generateDataPointsRight();
+
 
                 series = new LineGraphSeries<>(new DataPoint[]{
-                        new DataPoint(x[0], y[0]),
-                        new DataPoint(0, 0), //We have defined the origin to be the point where the light beam changes mediums
-                        new DataPoint(x[1], y[1])
+                        new DataPoint(0, 10),               //Hard-coded here because I'm lazy right now, I'll change it later
+                        new DataPoint(0, 0),                //We have defined the origin to be the point where the light beam changes mediums
+                        new DataPoint(DataPointsRight[0], DataPointsRight[1])
                 });
                 graph.addSeries(series);
             }
@@ -135,17 +136,55 @@ public class Snell_Interact extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                textViewN2.setText("Index of Refraction: " + progress);
+                textViewN2.setText("Index of Refraction N2: " + N2);
 
             }
         });
 
 
+    }
 
 
+    public double[] generateDataPointsRight() {
+        double x, y;
+        x = beamlength * Math.cos(Math.toRadians(theta2));        //Simple trigonometry & Snell's law dictates this relationship.
+        y = -1 * beamlength * Math.sin(Math.toRadians(theta2));         //Negative because we know that the beam of light is travelling downward
 
-
+        DataPointsRight[0] = x;
+        DataPointsRight[1] = y;
+        return DataPointsRight;
 
 
     }
+
+    /**
+     * These functions use Snell's law to calculate the angles (Much like Snell_Quix)
+     */
+
+
+    public void setThetaTwo() {
+        //For the sake of consistency, theta is converted to degrees here because the entire program is written assuming the global values of theta2 are in degrees
+        theta2 = Math.toDegrees(Math.asin(N1 / N2 * Math.sin(Math.toRadians(theta1))));
+
+
+    }
+
+    public void setN2(double N2) {
+        this.N2 = N2;
+    }
+
+    public void generateGraph() {
+
+        double x = 0;
+        double y =0;
+        double slope = DataPointsRight[1]/DataPointsRight[0];
+        while(x < DataPointsRight[0])
+        {
+
+
+
+        }
+
+    }
 }
+
