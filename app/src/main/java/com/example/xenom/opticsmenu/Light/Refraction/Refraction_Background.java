7c084b2ef94c6b1de97695a95a47d3dc16b37975
Tristan Ventura <tventura0297@gmail.com>
2017-06-06 15:23:58 -0400

    Added background activity for refraction modules

---
+++ app/src/main/java/com/example/xenom/opticsmenu/Light/Refraction/Refraction_Background.java
@@ -0,0 +1,15 @@
+package com.example.xenom.opticsmenu.Light.Refraction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.xenom.opticsmenu.R;
+
+public class Refraction_Background extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refraction__background);
+    }
+}
